01ae61f2fd8d98d019e3f9f039ec8adf1d261f9c
Adson Wesley <adsonxzx@gmail.com>
2019-11-17 09:47:29 -0300

    styling main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './style';
+
 export default function Main() {
-  return <h1>Main</h1>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositorios
+      </h1>
+
+      <Form>
+        <input type="text" placeholder="Adicionar repositório" />
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
